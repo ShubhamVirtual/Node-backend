edc1a93e85dad89ed7cfd7cc6064749fd3d1efa6
SudiptoVirtua <sudiptoroy@virtualemployee.com>
2022-07-08 13:54:15 +0530

    mongoDB connection

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
+const mongoose = require("mongoose");
+
+const URL='mongodb+srv://m001-student:m001-mongodb-basics@sandbox.xwpvdy5.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+   
+}).then(()=>{
+    console.log('MongoDB Connected successfully');
+}).catch(error=>{
+    console.log(error.message)
+});
+
 const app = express();
 const port = 8080;
 
